164cceb08d2f3154083e18020bba069122a1997f
Ryan Anderson <ryanflyan@hotmail.com>
2013-10-29 16:02:45 -0700

    altered the base guid gen method for more uniqueness.

--- combguid.js
+++ combguid.js
@@ -1,22 +1,25 @@
 function generateCombGuid() {
-  function g(d) {
-    for (var e = [0, 0, 0, 0, 0, 0, 0, 0], a = 0;a < e.length;a++) {
-      var b = d & 255;
-      e[a] = b;
-      d = (d - b) / 256;
+  function f(e) {
+    for (var c = [0, 0, 0, 0, 0, 0, 0, 0], b = 0;b < c.length;b++) {
+      var a = e & 255;
+      c[b] = a;
+      e = (e - a) / 256;
     }
-    return e;
+    return c;
   }
-  function h(a) {
-    for (var b = [], c = 0;c < a.length;c++) {
-      b.push((a[c] >>> 4).toString(16)), b.push((a[c] & 15).toString(16));
+  function g(a) {
+    for (var c = [], b = 0;b < a.length;b++) {
+      c.push((a[b] >>> 4).toString(16)), c.push((a[b] & 15).toString(16));
     }
-    return b.join("");
+    return c.join("");
   }
-  var f = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(a) {
-    var b = 16 * Math.random() | 0;
-    return("x" == a ? b : b & 3 | 8).toString(16);
-  }), f = f.substr(0, 24), a = moment().diff(moment([1900, 1, 1]), "days"), a = g(a).reverse(), a = a.slice(a.length - 2), a = h(a), b = Math.floor(moment().diff(moment().startOf("day")) / 3.333333), b = g(b).reverse(), b = b.slice(b.length - 4), b = h(b);
-  return f + a + b;
-}
-;
+  var h = function() {
+    var a = (new Date).getTime();
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      var b = (a + 16 * Math.random()) % 16 | 0;
+      a = Math.floor(a / 16);
+      return("x" == c ? b : b & 7 | 8).toString(16);
+    });
+  }().substr(0, 24), a = moment().diff(moment([1900, 1, 1]), "days"), a = f(a).reverse(), a = a.slice(a.length - 2), a = g(a), d = Math.floor(moment().diff(moment().startOf("day")) / 3.333333), d = f(d).reverse(), d = d.slice(d.length - 4), d = g(d);
+  return h + a + d;
+};
